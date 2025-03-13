8afb717e393cc8f4f737cb94360366c649cdd666
Helgagoit <mixanik@ukr.net>
2025-03-13 09:20:09 +0200

    Add files via upload

--- Homework_3_2.py
+++ Homework_3_2.py
@@ -1,14 +1,13 @@
 import random 
 
 def get_numbers_ticket (min, max, quantity):
-    if min < 1 or max > 1000 or not (min <= quantity <= max):
+    if min < 1 or max > 1000 or quantity < 1 or quantity > (max - min + 1):
         return []
     numbers = set ()
     
-    while len(numbers) < quantity:
+    while len(numbers) < quantity: 
         numbers.add(random.randint(min, max))
     return sorted(numbers)
-
-   
-lottery_numbers = get_numbers_ticket(1, 555, 10)
+ 
+lottery_numbers = get_numbers_ticket(10, 15, 5)
 print("Ваші лотерейні числа:", lottery_numbers)
